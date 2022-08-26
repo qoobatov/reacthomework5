612ffc016982c7cac405094753a01673390188d6
Erkin Kubatov <qoobatov@gmail.com>
2022-08-26 16:59:06 +0600

    create hw 5

---
+++ src/components/taskList/TaskList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TaskList.css";
+import Task from "../task/Task";
+
+class TaskList extends React.Component {
+  render() {
+    return (
+      <div className="tasks-list-container">
+        <div className="tasks-list">
+          {this.props.tasks.map((task) => (
+            <Task
+              key={task.key}
+              text={task.text}
+              id={task.id}
+              removeTask={this.props.removeTask}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TaskList;
